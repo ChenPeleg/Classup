526e028c135bd9534942809e48880617adb2bde7
חן פלג <chenpeleg@gmail.com>
2020-05-23 13:12:07 +0300

    github pages addon

---
+++ build/precache-manifest.06d21a860e572aa9e283120f637bf7fb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155aba95f9b3c2143d035d5888c8e7f8",
+    "url": "/Classup/index.html"
+  },
+  {
+    "revision": "885199d4c4c86b05d536",
+    "url": "/Classup/static/css/main.598f2e8f.chunk.css"
+  },
+  {
+    "revision": "9916bedcdb2c239cf097",
+    "url": "/Classup/static/js/2.7e249ff3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Classup/static/js/2.7e249ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885199d4c4c86b05d536",
+    "url": "/Classup/static/js/main.c218621f.chunk.js"
+  },
+  {
+    "revision": "b68a578f3a1cc3877e23",
+    "url": "/Classup/static/js/runtime-main.e526d175.js"
+  },
+  {
+    "revision": "3806fe596449a857e77b4a6754a8e5d5",
+    "url": "/Classup/static/media/Correct.3806fe59.mp3"
+  },
+  {
+    "revision": "46646479523f49ef5af50b70dcf12668",
+    "url": "/Classup/static/media/KulimPark-Regular.46646479.ttf"
+  },
+  {
+    "revision": "c6004be49628b1226eb31b068348d24f",
+    "url": "/Classup/static/media/LuckiestGuy-Regular.c6004be4.ttf"
+  },
+  {
+    "revision": "5612b2bdd2b319493990aeb6c195367f",
+    "url": "/Classup/static/media/Wrong.5612b2bd.mp3"
+  },
+  {
+    "revision": "dd06661253bfa18e7a556851a3d081bb",
+    "url": "/Classup/static/media/check-mark.dd066612.svg"
+  },
+  {
+    "revision": "baa55592fa1d7ca1e5f7e40848057d9d",
+    "url": "/Classup/static/media/menu-button.baa55592.svg"
+  },
+  {
+    "revision": "e9a05bdf6dfd7b1a1740e55f87f4e4f9",
+    "url": "/Classup/static/media/x-mark.e9a05bdf.svg"
+  },
+  {
+    "revision": "b3c65bf0d5c4974fcca1527b2c4849a1",
+    "url": "/Classup/static/media/xbutton.b3c65bf0.png"
+  }
+]);
