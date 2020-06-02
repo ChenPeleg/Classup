aaa35a53eaa2d99dc05f27a5749fda7675e71726
חן פלג <chenpeleg@gmail.com>
2020-06-02 14:44:58 +0300

    after build

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -7,39 +7,39 @@ import "./Layout.scss";
 import LoaderAnimation from "../../components/UI/LoaderAnimation/LoaderAnimation";
 
 class Layout extends Component {
-  state = { AllQuestions: false, soundOn: true };
-  soundHandler = () => {
-    this.setState({ ...this.state, soundOn: !this.state.soundOn });
-  };
-  componentDidMount() {
-    const url = this.props.questionsUrl;
-    axios.get(url, { crossdomain: true }).then((res) => {
-      this.setState({ ...this.state, AllQuestions: res.data });
-    }).catch(() => { this.setState({ ...this.state, AllQuestions: allquestions }) });
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <Navigation
-          AllQuestions={
-            this.state.AllQuestions
-              ? this.state.AllQuestions
-              : { meta: { name: "..." } }
-          }
-          soundOn={this.state.soundOn}
-          soundHandler={this.soundHandler}
-        />
+    state = { AllQuestions: false, soundOn: true };
+    soundHandler = () => {
+        this.setState({...this.state, soundOn: !this.state.soundOn });
+    };
+    componentDidMount() {
+        const url = this.props.questionsUrl;
+        axios.get(url, { crossdomain: true, maxRedirects: 2 }).then((res) => {
+            this.setState({...this.state, AllQuestions: res.data });
+        }).catch(() => { this.setState({...this.state, AllQuestions: allquestions }) });
+    }
+    render() {
+        return ( <
+            React.Fragment >
+            <
+            Navigation AllQuestions = {
+                this.state.AllQuestions ?
+                this.state.AllQuestions : { meta: { name: "..." } }
+            }
+            soundOn = { this.state.soundOn }
+            soundHandler = { this.soundHandler }
+            />
 
-        {this.state.AllQuestions ? (
-          <LearningContainer
-            AllQuestions={this.state.AllQuestions}
-            soundOn={this.state.soundOn}
-          />
-        ) : (
-            <LoaderAnimation />
-          )}
-      </React.Fragment>
-    );
-  }
+            {
+                this.state.AllQuestions ? ( <
+                    LearningContainer AllQuestions = { this.state.AllQuestions }
+                    soundOn = { this.state.soundOn }
+                    />
+                ) : ( <
+                    LoaderAnimation / >
+                )
+            } <
+            /React.Fragment>
+        );
+    }
 }
 export default Layout;
